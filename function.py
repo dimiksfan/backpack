0df2be71df1be931bbfa2107be33b66db91ea65c
Dima Suraganov <92138532+dimiksfan@users.noreply.github.com>
2023-02-17 11:20:52 +0500

    Update function.py

--- function.py
+++ function.py
@@ -8,10 +8,21 @@ def binMask(lst, maxW=100):
             if bool(i & mask):
                 combo += [lst[pos]]
                 s += int(lst[pos]['weight'])
-        if s <= maxW: combs =+ [combo]
-        
-        
+        if s <= maxW: combs =+ [combo]              
     return findMax(combs)
+
+
+def recSearch(lst, maxW=100):
+    def rec(combo, deep):
+        if deep == len(lst):  # точка останова
+            if (noWay(combo, maxW)):
+                combs.append(combo)
+        else:  # шаги рекурсии
+            rec(combo+[lst[deep]], deep+1)
+            rec(combo, deep+1)
+            combs = []
+            rec([], 0)
+            return findMax(combs)
 
 
 def findMax(lst):
@@ -25,3 +36,12 @@ def findMax(lst):
                 maxP = curP
                 answ = item
     return answ
+
+
+def noWay(lst, maxW=100):
+    sumW = 0
+    for i in range (len(lst)):
+        sumW = sumW + int(lst[i]['weight'])
+        if sumW <= maxW:
+            return True
+        return False
