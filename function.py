be23bdc98ae70f45641b008f5b7dd1a8a6b562b0
Dima Suraganov <92138532+dimiksfan@users.noreply.github.com>
2023-02-17 11:04:51 +0500

    Update function.py

--- function.py
+++ function.py
@@ -1,4 +1,4 @@
-def binMask(lst):
+def binMask(lst, maxW=100):
     combs = []
     n = len(lst)
     for i in range(1 << n):  # combo = 9 -> 1001
@@ -9,19 +9,19 @@ def binMask(lst):
                 combo += [lst[pos]]
                 s += int(lst[pos]['weight'])
         if s <= maxW: combs =+ [combo]
-     return findMax(combs)
-  
-  
+        
+        
+    return findMax(combs)
+
+
 def findMax(lst):
-   maxP = 0
-   answ =[]
-   for item in lst:
-       curP = 0 
-       for pos in range(len[item]):
-           curP =+ int(item[pos]['price'])
-           if curP > maxP:
-               maxP = curP
-               answ = item
-   return answ
-
-
+    maxP = 0
+    answ =[]
+    for item in lst:
+        curP = 0 
+        for pos in range(len[item]):
+            curP =+ int(item[pos]['price'])
+            if curP > maxP:
+                maxP = curP
+                answ = item
+    return answ
