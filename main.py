15afdbb6ef66e9844453ea0b25fe67b9d7ed44d3
Dima Suraganov <92138532+dimiksfan@users.noreply.github.com>
2023-02-17 10:55:42 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,23 @@
+from readerCsv import getLst
+from function import binMask, recSearch
 
+def elemSum(lst):
+    sumP = 0
+    for i in range(len(lst)):
+        sumP = sumP + int(lst[i]['price'])
+    return sumP
+
+
+def findAnsw(lst, maxW):
+
+    answ = binMask(lst, maxW)
+    print("Бин. маски:\n",answ,'\n',"Сумма:", elemSum(answ),'\n')
+    answ = recSearch(lst, maxW)
+    print("Рекурсия:\n",answ,'\n',"Сумма:", elemSum(answ),'\n')
+
+
+
+lst = getLst('./numbers.csv')
+maxW = 100
+answ = []
+findAnsw(lst, maxW)
